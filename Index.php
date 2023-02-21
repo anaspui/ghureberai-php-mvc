0970853cef477704127b5c3127a8287ee3f57a9b
Mohammad Omar <64026794+anaspui@users.noreply.github.com>
2023-02-21 21:02:26 +0600

    Working on Button,Div,CSS

---
+++ Index.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+    <style>
+        .NavBar {
+            vertical-align: ;
+            border: 1px black solid;
+            height: 30px;
+            width: 90%;
+            margin: auto;
+            padding-top: 1rem;
+            padding-bottom: 1rem;
+        }
+
+        .Body {
+            border: 1px black solid;
+            height: 800px;
+            width: 90%;
+            margin: auto;
+            margin-top: 10px;
+        }
+
+        .NavBtn {
+            /* background-color: black; */
+            border-color: white;
+            text-align: center;
+            text-decoration: None;
+            height: 30px;
+            width: 100px;
+            cursor: pointer;
+
+        }
+    </style>
+</head>
+
+<body>
+    <div class="NavBar">
+        Navbar
+        <Button class="NavBtn">Home</Button>
+        <Button class="NavBtn">About</Button>
+        <Button class="NavBtn" style=" margin-left: 75%;">Login</Button>
+    </div>
+    <div class="Body">
+        Body
+    </div>
+</body>
+
+</html>
